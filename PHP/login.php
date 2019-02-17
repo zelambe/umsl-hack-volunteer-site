4de8c38620cd3a6f359b6d1af996fa5cda6be514
Niamke Dozier <s1niamke@gmail.com>
2019-02-16 19:34:38 -0600

    updates!

--- PHP/login.php
+++ PHP/login.php
@@ -1,7 +1,19 @@
     <?php
-        require_once '../PHP/dbconfig.php';
-
-        
-
+    
+        require_once 'dbconfig.php';
+        try {
+        $pdo = new PDO(MYSQL_CONNECTION_STRING, 
+        DB_USER, 
+        DB_PWD); $pdo->setAttribute(PDO::ATTR_ERRMODE, 
+                                PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT * FROM UserLogTest";
+            $result = $pdo->query($sql);
+            
+            $pdo = null;
+            echo "nhello";
+        }
+            catch(PDOException $e){
+                var_dump($e);
+            }
 
     ?>
